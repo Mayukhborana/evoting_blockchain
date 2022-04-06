d5fd30d0bcf5b095b0cc2db586f0d9b2eb498e19
Mayukhborana <mayukhborana@gmail.com>
2022-04-06 09:40:29 +0530

    2nd' commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# E-Voting 
+
+This is an online web based voting system which is built on the top of Blockchain technology.
+
+how to use?
+
+just clone this repo to your device, go to evoting_blockchain directory and run command "npm insatall" it will install all the dependencies.
+after that run "npm start".
+## Why E-Voting
+  - Voters can vote from anywhere/ anytime 
+  - Best in Security
+  - Fast Results
+  - No fund wasted by the government to organize voting environment
+  - Imposible to vote twice
+  
+## Features:
+
+## Admin can 
+  - See overall summary chart of votes
+  - Manage voters(CRUD)
+  - Manage candidates (CRUD)
+  - Manage Positions (CRUD)
+  - Update/Change Ballot Title
+
+## Voters can
+  - Registration
+  - Login
+  - Verification with their email or mobile no. credentials
+  - Votes for their favourite candidates
+  - View candidates they voted for
+
---
+++ app.js
@@ -0,0 +1,42 @@
+const PORT = process.env.PORT || 4000;
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+app.set('view engine', 'ejs');
+// app.engine("html", require("ejs").renderFile);
+// const staticpath= path.join(__dirname,"/public");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+//app.use(favicon(path.join(__dirname,'public','favicon.ico'))); //Use of favicon in future
+console.log("Hii E-voting");
+
+
+
+//Home page
+app.get("/", function (req, res) {
+    res.render("home");
+});
+
+// about page
+app.get('/about', function(req, res) {
+  res.render('about');
+});
+
+// registeration page
+app.get("/signup", function (req, res) {
+  res.render("signup");
+});
+
+// login form
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+  
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});// http://localhost:8099
+
+
+
